b70b51831a8f0203a6e6c6196cb281321557148f
schub <schub@NITRO>
2024-04-28 17:26:52 +0530

    Commit

---
+++ kimieeee/src/kimieeee/testo.java
@@ -0,0 +1,10 @@
+package kimieeee;
+
+public class testo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
---
+++ kimieeee/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author schub
+ *
+ */
+module kimieeee {
+}
